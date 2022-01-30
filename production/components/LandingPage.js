1cb01f18da69698aaa723c63ec561049718e3911
oyama Makapela <oyama.makapela@gmail.com>
2022-01-29 20:47:04 -0800

    initial Commit by Oyama

---
+++ production/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+
+function LandingPage({data}) {
+    
+  return <div>
+    
+      <Image src='/Image.jpg' width={200} height={200} alt=''/>
+
+  </div>;
+}
+
+export default LandingPage;
