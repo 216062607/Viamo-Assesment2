2d3fd252988b18ee002cfad9fa84aaeceb398f05
oyama Makapela <oyama.makapela@gmail.com>
2022-01-31 19:58:02 -0800

    Adding CSS grid

--- production/components/LandingPage.js
+++ production/components/LandingPage.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import Image from 'next/image';
 
-function LandingPage({data}) {
+function LandingPage({items}) {
     
-  return <div>
-    
-      <Image src='/Image.jpg' width={200} height={200} alt=''/>
+  return <div className='Container'>
+          <div>
+         <img src='/Image.jpg' width={416} height={416} alt=''/>
+         </div>
+         <div>
+           <div className='container1'>
+             <img src='/logo.png' width={50}  height={50} alt=''/>
 
-  </div>;
+             <div>
+               <h3>Free shipping</h3>
+             </div>
+             <div>
+               <h3>20% off</h3>
+             </div>
+
+           </div>
+         </div>
+
+         <div className="container3">
+            <div className="button">
+              <p>Log in to purchase</p>
+              </div>
+              <br/>
+              <div className="button1">
+                
+                <img src='/icons8-envelope.png' alt=''/>
+              <p>Contact the Supplier</p>
+                
+            </div>
+         </div>
+
+        </div>;
 }
 
 export default LandingPage;
